38b87caec765b5e4fe14722fd23b8291849eab96
Manny Lee <many221@gmail.com>
2022-01-10 22:25:37 -0800

    adding packages

---
+++ src/main/java/com/cd/peddler/PeddlerApplication.java
@@ -0,0 +1,13 @@
+package com.cd.peddler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeddlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeddlerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cd/peddler/PeddlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cd.peddler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeddlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
